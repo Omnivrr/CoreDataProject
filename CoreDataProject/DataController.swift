f474c974d343c4a2255bde3ea04823102c3d653a
Omnivrr <Omnivrr@gmail.com>
2023-06-30 20:25:19 +0100

    Project Commenced.

---
+++ CoreDataProject/DataController.swift
@@ -0,0 +1,22 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Bukhari Sani on 30/06/2023.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { desccription, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+            
+        }
+    }
+}
---
+++ CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Bukhari Sani on 30/06/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
