e5acad353075bbafde759f46e5482c6141f95eb3
Omnivrr <Omnivrr@gmail.com>
2023-07-01 23:56:54 +0100

    s

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -4,12 +4,42 @@
 //
 //  Created by Bukhari Sani on 30/06/2023.
 //
-
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    
+    
+    @FetchRequest(sortDescriptors: [], predicate: nil) var ships: FetchedResults<Ship>
+    
     var body: some View {
         VStack {
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
+            }
+            
+            Button("Add Examples")
+            let ship1 = Ship(context: moc)
+            ship1.name = "Enterprise"
+            ship1.universe = "Star Trek"
+            
+            let ship2 = Ship(context: moc)
+            ship2.name = "Defiant"
+            ship2.universe = "Star Trek"
+            
+            let ship3 = Ship(context: moc)
+            ship3.name = "Millenium Falcon"
+            ship3.universe = "Star Wars"
+            
+            let ship4 = Ship(context: moc)
+            ship4.name = "Executor"
+            ship4.universe = "Star Wars"
+
+            try? moc.save()
+
+            
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
